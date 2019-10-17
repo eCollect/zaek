c1e5dd1c0e927f7e75e6335cd90281e647d6f6c0
nanov <mitko.n@gmail.com>
2019-10-17 17:47:35 +0300

    wip: implement RPC pattern

---
+++ examples/single-process/worker.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/* eslint-disable no-console */
+
+const zaek = require('../../lib/index');
+
+class EndAwaitor {
+	constructor() {
+		this._resolve = null;
+		this._promise = new Promise((resolve) => {
+			this._resolve = resolve;
+		});
+	}
+
+	async await() {
+		return this._promise;
+	}
+
+	end() {
+		this._resolve();
+	}
+}
+
+async function run() {
+	const broker = await zaek.connect({
+		user: 'guest',
+		pass: 'guest',
+		username: 'guest',
+		password: 'guest',
+		server: ['127.0.0.1'],
+		port: 5672,
+		hostname: 'localhost',
+	});
+
+	const onError = (err) => {
+		console.error(err);
+		process.exit(1);
+	};
+
+	broker.once('error', onError);
+
+	const readStream = await broker.worker('test:zaek:worker').createReadStream({ prefetch: 1 });
+	readStream.on('error', onError);
+
+	const endAwaitor = new EndAwaitor();
+
+	readStream.on('data', (message) => {
+		if (!message.body) {
+			message.ack();
+			return endAwaitor.end();
+		}
+
+		console.log(`---[work received wid: ${message.body.i}  pid: ${process.pid}]----`);
+		console.log(message);
+		return setTimeout(() => message.ack(), Math.random() * 10); // simulate work
+	});
+
+	const writeStream = await broker.worker('test:zaek:worker').createWriteStream();
+	writeStream.on('error', onError);
+
+	for (let i = 0; i < 100; i++)
+		writeStream.write({
+			body: { i },
+		});
+
+	writeStream.write({
+		body: null,
+	});
+
+	/*
+	writeStream.end(async () => {
+		await broker.close();
+	});
+	*/
+
+	await endAwaitor.await();
+	process.exit(0);
+
+	// stream.end(err => process.exit(err ? 1 : 0));
+
+	// process.exit(0);
+}
+
+run();
